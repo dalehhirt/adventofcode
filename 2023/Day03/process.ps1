8527ebc18bf4cc71e95b787614035dbf5f08f8fe
Dale Hirt <2246127+dalehhirt@users.noreply.github.com>
2023-11-30 14:00:59 -0800

    fix: initial 2023 files ready to go

---
+++ 2023/Day03/process.ps1
@@ -0,0 +1,26 @@
+<#
+.Description
+This script runs.
+#>
+[CmdletBinding(SupportsShouldProcess=$true)]
+param()
+begin {
+  function log() {
+    write-host ">>> [$($env:COMPUTERNAME)]" ((Get-Date).ToUniversalTime().ToString('u')) "$args" -ForeGroundColor Green
+  }
+  
+  function log-verbose() {
+    write-verbose ">>> [$($env:COMPUTERNAME)] $((Get-Date).ToUniversalTime().ToString('u')) $args"
+  }
+  
+  function log-error() {
+    write-error ">>> [$($env:COMPUTERNAME)] $((Get-Date).ToUniversalTime().ToString('u')) $args"
+  }
+
+  $InputFile = Resolve-Path (Join-Path $PSScriptRoot "input.txt")
+}
+process {
+    get-content $InputFile
+}
+end {
+}
