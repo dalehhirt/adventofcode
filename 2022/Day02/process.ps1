6a9b2f6ba8875bec500ae073c0d6244261323001
Dale Hirt <2246127+dalehhirt@users.noreply.github.com>
2023-12-01 08:37:46 -0800

    fix: updating to new process.ps1

--- 2022/Day02/process.ps1
+++ 2022/Day02/process.ps1
@@ -42,11 +42,60 @@ begin {
     write-error ">>> [$($env:COMPUTERNAME)] $((Get-Date).ToUniversalTime().ToString('u')) $args"
   }
 
+  function Get-Part1Answer {
+    [CmdletBinding()]
+    param (
+      [Parameter(ValueFromPipeline=$true)]
+      [string[]]
+      $lines
+    )
+    
+    begin {
+      $returnValue = ""
+    }
+    
+    process {
+      foreach ($line in $lines) {
+      }
+    }
+    
+    end {
+      return $returnValue
+    }
+  }
+
+  function Get-Part2Answer {
+    [CmdletBinding()]
+    param (
+      [Parameter(ValueFromPipeline=$true)]
+      [string[]]
+      $lines
+    )
+    
+    begin {
+      $returnValue = ""
+    }
+    
+    process {
+      foreach ($line in $lines) {
+      }
+    }
+    
+    end {
+      return $returnValue
+    }
+  }
+
+  #-----------------
+  # Global Variables
   $InputFile = Resolve-Path (Join-Path $PSScriptRoot "input.txt")
 }
 process {
-  get-content $InputFile
+  $lines = get-content $InputFile
 
+  log "Part 1 Answer:" ($lines | Get-Part1Answer)
+
+  log "Part 2 Answer:" ($lines | Get-Part2Answer)
 }
 end {
 }
