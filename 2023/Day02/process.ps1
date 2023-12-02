01a3e84963dbcf6a236c4ed6f302f6499b6d9279
Dale Hirt <2246127+dalehhirt@users.noreply.github.com>
2023-12-01 21:56:24 -0800

    feat: part 1 Day 2

--- 2023/Day02/process.ps1
+++ 2023/Day02/process.ps1
@@ -15,14 +15,58 @@ begin {
     
     begin {
       $returnValue = ""
+      $regex = "(\d+):"
+      # 12 red cubes, 13 green cubes, and 14 blue cubes
+      $totalRed = 12
+      $totalGreen = 13
+      $totalBlue = 14
+      $games = @()
     }
     
     process {
       foreach ($line in $lines) {
+        $validGame = $true
+        $game = [regex]::Matches($line, $regex)[0]
+        log-verbose "Game Number" $game.Groups[1]
+        $handfuls = ($line -split ":")[1] -split ";"
+        foreach ($handful in $handfuls) {
+          #log-verbose "Handful:" $handful
+          $cubes = $handful -split ","
+          foreach ($cube in $cubes) {
+            #log-verbose "Cube:" $cube
+            $values = -split $cube
+            #log-verbose $values[1] $values[0]
+            switch ($values[1]) {
+              "green" {
+                if($totalGreen -lt [int]$values[0]){
+                  log-verbose "Green: Expected:" $totalBlue "Actual:" $values[0]
+                  $validGame = $false
+                }
+              }
+              "red" {
+                if($totalRed -lt [int]$values[0]){
+                  log-verbose "Red: Expected:" $totalBlue "Actual:" $values[0]
+                  $validGame = $false
+                }
+              }
+              "blue" {
+                if($totalBlue -lt [int]$values[0]){
+                  log-verbose "Blue: Expected:" $totalBlue "Actual:" $values[0]
+                  $validGame = $false
+                }
+              }
+              Default {}
+            }
+          }
+        }
+        if($validGame) {
+          $games += $game.Groups[1]
+        }
       }
     }
     
     end {
+      $returnValue =  ($games.value | measure-object -sum).Sum
       return $returnValue
     }
   }
