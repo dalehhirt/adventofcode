24794487cd7e8ecce1513c715f29dd52dd41d9f8
Dale Hirt <2246127+dalehhirt@users.noreply.github.com>
2023-12-02 23:03:09 -0800

    feat: part 2 of day 3 of 2023

--- 2023/Day03/process.ps1
+++ 2023/Day03/process.ps1
@@ -7,7 +7,7 @@ https://adventofcode.com/2023/day/3
 [CmdletBinding(SupportsShouldProcess=$true)]
 param()
 begin {
-  function Match-Symbols {
+  function Match-SymbolsPart1 {
     [CmdletBinding()]
     param (
       [Parameter(ValueFromPipeline=$true)]
@@ -46,7 +46,7 @@ begin {
     }
   }
 
-  function Process-PartNumbers {
+  function Process-PartNumbersPart1 {
     [CmdletBinding()]
     param (
       $Digits,
@@ -83,15 +83,15 @@ begin {
         }
 
         if ($null -ne $BeforeLine) {
-          $returnValues += @($DigitsMatches | Match-Symbols -Symbols $BeforeLine)
+          $returnValues += @($DigitsMatches | Match-SymbolsPart1 -Symbols $BeforeLine)
         }
 
         if ($null -ne $CurrentLine) {
-          $returnValues += @($DigitsMatches | Match-Symbols -Symbols $CurrentLine)
+          $returnValues += @($DigitsMatches | Match-SymbolsPart1 -Symbols $CurrentLine)
         }
 
         if ($null -ne $AfterLine) {
-          $returnValues += @($DigitsMatches | Match-Symbols -Symbols $AfterLine)
+          $returnValues += @($DigitsMatches | Match-SymbolsPart1 -Symbols $AfterLine)
         }
       }
     }
@@ -141,8 +141,125 @@ begin {
     }
     
     end {
-      $returnValue = (Process-PartNumbers -Digits $Digits -Symbols $Symbols | Measure-Object -Sum).Sum
+      $returnValue = (Process-PartNumbersPart1 -Digits $Digits -Symbols $Symbols | Measure-Object -Sum).Sum
       return $returnValue
+    }
+  }
+
+function Check-MatchIndexPart2() {
+  param([Parameter(ValueFromPipeline=$true)]
+  [object[]]
+  $Matches,
+  $matchIndex)
+
+  begin {
+    $returnValues = $()
+  }
+  
+  process {
+    foreach ($match in $Matches) {
+
+      $matchingIndexes = @()
+      # one before matches for diagonal as well        
+      # one after matches for diagonal as well
+      $matchingIndexes += ($match.Index - 1)..($match.Index + $match.Length)
+    
+      if($matchingIndexes -contains $matchIndex){
+        $returnValues += @($match)
+      }
+    }
+  }
+
+  end {
+    return $returnValues
+  }
+}
+
+function Match-SymbolsPart2 {
+    [CmdletBinding()]
+    param (
+      [Parameter(ValueFromPipeline=$true)]
+      [object[]]
+      $Matches,
+      $DigitsBefore,
+      $DigitsCurrent,
+      $DigitsAfter
+    )
+    
+    begin {
+      $returnValues = $()
+    }
+    
+    process {
+      foreach ($match in $Matches) {
+        $DigitsThatCover = @()
+        if ($null -ne $DigitsBefore) {
+          $DigitsThatCover += @($DigitsBefore | Check-MatchIndexPart2 -matchIndex $match.Index)
+        }
+
+        $DigitsThatCover += @($DigitsCurrent | Check-MatchIndexPart2 -matchIndex $match.Index)
+
+        if ($null -ne $DigitsAfter) {
+          $DigitsThatCover += @($DigitsAfter | Check-MatchIndexPart2 -matchIndex $match.Index)
+        }
+
+        # Filter out nulls
+        $DigitsThatCover = ($DigitsThatCover | Where-Object {$null -ne $_})
+
+        # Only do this if we equal 2
+        if($DigitsThatCover.Count -eq 2) {
+          $returnValues += @([int]$DigitsThatCover[0].Value * [int]$DigitsThatCover[1].Value)
+        }
+      }
+    }
+    
+    end {
+      return $returnValues
+    }
+  }
+
+  function Process-PartNumbersPart2 {
+    [CmdletBinding()]
+    param (
+      $Digits,
+      $Symbols
+    )
+    
+    begin {
+      $returnValues = $()
+      $orderedKeys = $Symbols.keys | Sort-Object
+    }
+    
+    process {
+      foreach ($key in $orderedKeys) {
+        log-verbose "Checking line" $key
+        $SymbolsMatches = $Symbols[$key]
+        if($SymbolsMatches.Count -eq 0) {
+          # skip
+          continue
+        }
+
+        $BeforeLine = $null
+        if($Digits.ContainsKey($key - 1)) {
+          $BeforeLine = $Digits[$key - 1]
+        }
+
+        $CurrentLine = $null
+        if($Digits.ContainsKey($key)) {
+          $CurrentLine = $Digits[$key]
+        }
+
+        $AfterLine = $null
+        if($Digits.ContainsKey($key + 1)) {
+          $AfterLine = $Digits[$key + 1]
+        }
+
+        $returnValues += @($SymbolsMatches | Match-SymbolsPart2 -DigitsBefore $BeforeLine -DigitsCurrent $CurrentLine -DigitsAfter $AfterLine)
+      }
+    }
+    
+    end {
+      return $returnValues
     }
   }
 
@@ -156,14 +273,36 @@ begin {
     
     begin {
       $returnValue = ""
+      $Symbols = @{}
+      $Digits = @{}
+      $reDigits = "\d+"
+      $lineNumber = 0
     }
     
     process {
       foreach ($line in $lines) {
+        $lineNumber++
+        
+        log-verbose "Line:" $line
+        $digitsMatches = [regex]::Matches($line, $reDigits)
+        $Digits.Add($lineNumber, $digitsMatches)
+
+        $symbolMatches = for ($i = 0; $i -lt $line.Length; $i++) {
+          log-verbose "Line[i] =" $line[$i]
+          if($line[$i] -eq "*") {
+            [PSCustomObject]@{
+              Value = $line[$i]
+              Index = $i
+              Length = 1
+            }
+          }
+        }
+        $Symbols.Add($lineNumber, $symbolMatches)
       }
     }
     
     end {
+      $returnValue = (Process-PartNumbersPart2 -Digits $Digits -Symbols $Symbols | Measure-Object -Sum).Sum
       return $returnValue
     }
   }
