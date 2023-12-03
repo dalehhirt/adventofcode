48595b924f60352d466681877f7efa5926fd277c
Dale Hirt <2246127+dalehhirt@users.noreply.github.com>
2023-12-02 22:17:07 -0800

    feat: Part 1 of day 3 2023

--- 2023/Day03/process.ps1
+++ 2023/Day03/process.ps1
@@ -2,11 +2,105 @@
 .Description
 This script runs.
 .LINK
-<Replace with link to day problem>
+https://adventofcode.com/2023/day/3
 #>
 [CmdletBinding(SupportsShouldProcess=$true)]
 param()
 begin {
+  function Match-Symbols {
+    [CmdletBinding()]
+    param (
+      [Parameter(ValueFromPipeline=$true)]
+      [object[]]
+      $Matches,
+      $Symbols
+    )
+    
+    begin {
+      $SymbolsPositions = @($Symbols.Index)
+      $returnValues = $()
+    }
+    
+    process {
+      foreach ($match in $Matches) {
+        $matchingIndexes = @()
+        
+        # one before matches for diagonal as well        
+        # one after matches for diagonal as well
+        $matchingIndexes += ($match.Index - 1)..($match.Index + $match.Length)
+        
+        $found = $false
+        $matchingIndexes | ForEach-Object {
+          if($SymbolsPositions -contains $_){
+            $found = $true
+          }
+        }
+        if($found) {
+          $returnValues += @($match.Value)
+        }
+      }
+    }
+    
+    end {
+      return $returnValues
+    }
+  }
+
+  function Process-PartNumbers {
+    [CmdletBinding()]
+    param (
+      $Digits,
+      $Symbols
+    )
+    
+    begin {
+      $returnValues = $()
+      $orderedKeys = $Digits.keys | Sort-Object
+    }
+    
+    process {
+      foreach ($key in $orderedKeys) {
+        log-verbose "Checking line" $key
+        $DigitsMatches = $Digits[$key]
+        if($DigitsMatches.Count -eq 0) {
+          # skip
+          continue
+        }
+
+        $BeforeLine = $null
+        if($Symbols.ContainsKey($key - 1)) {
+          $BeforeLine = $Symbols[$key - 1]
+        }
+
+        $CurrentLine = $null
+        if($Symbols.ContainsKey($key)) {
+          $CurrentLine = $Symbols[$key]
+        }
+
+        $AfterLine = $null
+        if($Symbols.ContainsKey($key + 1)) {
+          $AfterLine = $Symbols[$key + 1]
+        }
+
+        if ($null -ne $BeforeLine) {
+          $returnValues += @($DigitsMatches | Match-Symbols -Symbols $BeforeLine)
+        }
+
+        if ($null -ne $CurrentLine) {
+          $returnValues += @($DigitsMatches | Match-Symbols -Symbols $CurrentLine)
+        }
+
+        if ($null -ne $AfterLine) {
+          $returnValues += @($DigitsMatches | Match-Symbols -Symbols $AfterLine)
+        }
+      }
+    }
+    
+    end {
+      return $returnValues
+    }
+  }
+
   function Get-Part1Answer {
     [CmdletBinding()]
     param (
@@ -17,14 +111,37 @@ begin {
     
     begin {
       $returnValue = ""
+      $Symbols = @{}
+      $notSymbol = "."
+      $Digits = @{}
+      $reDigits = "\d+"
+      $lineNumber = 0
     }
     
     process {
       foreach ($line in $lines) {
+        $lineNumber++
+        
+        log-verbose "Line:" $line
+        $digitsMatches = [regex]::Matches($line, $reDigits)
+        $Digits.Add($lineNumber, $digitsMatches)
+
+        $symbolMatches = for ($i = 0; $i -lt $line.Length; $i++) {
+          log-verbose "Line[i] =" $line[$i]
+          if(($line[$i] -notmatch "\d") -and ($line[$i] -ne $notSymbol)) {
+            [PSCustomObject]@{
+              Value = $line[$i]
+              Index = $i
+              Length = 1
+            }
+          }
+        }
+        $Symbols.Add($lineNumber, $symbolMatches)
       }
     }
     
     end {
+      $returnValue = (Process-PartNumbers -Digits $Digits -Symbols $Symbols | Measure-Object -Sum).Sum
       return $returnValue
     }
   }
