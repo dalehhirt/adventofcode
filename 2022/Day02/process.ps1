d28f2cf2dc573595f43999725ae3d5f9f54d535e
Dale Hirt <2246127+dalehhirt@users.noreply.github.com>
2023-12-02 16:34:04 -0800

    feat: initial day 2 2022

--- 2022/Day02/process.ps1
+++ 2022/Day02/process.ps1
@@ -15,16 +15,34 @@ begin {
     
     begin {
       $returnValue = ""
+      $scores = $()
     }
     
     process {
       foreach ($line in $lines) {
         $choices = -split $line
-        log-verbose "First Player $($choices[0]) <-> Second Player $($choices[1])"
+
+        $leftHand = $leftHandValues[$choices[0]]
+        $rightHand = $rightHandValues[$choices[1]]
+        
+        log-verbose "First Player $leftHand <-> Second Player $rightHand"
+        
+        $score = $baseValues[$rightHand].Value
+        if($leftHand -eq $rightHand) {
+          $score += $drawScore
+        }
+        elseif ($baseValues[$leftHand].Beats -eq $rightHand) {
+          $score += $lossScore
+        }
+        else {
+          $score += $winScore
+        }
+        $scores += $score
       }
     }
     
     end {
+      $returnValue = ($scores | Measure-Object -Sum).Sum
       return $returnValue
     }
   }
@@ -61,6 +79,26 @@ begin {
   log-verbose "Input file 1 path: $InputFile1"
   $InputFile2 = Resolve-Path (Join-Path $PSScriptRoot "input2.txt")
   log-verbose "Input file 2 path: $InputFile2"
+
+  $baseValues = @{
+    "rock" = [PSCustomObject]@{
+      "Value" = 1
+      "Beats" = "scissors"
+    }
+    "paper" = [PSCustomObject]@{
+      "Value" = 2
+      "Beats" = "rock"
+    }
+    "scissors" = [PSCustomObject]@{
+      "Value" = 3
+      "Beats" = "paper"
+    }
+  }
+  $leftHandValues = @{"A"="rock"; "B"="paper"; "C" = "scissors"}
+  $rightHandValues = @{"X"="rock"; "Y"="paper"; "Z" = "scissors"}
+  $winScore = 6
+  $lossScore = 0
+  $drawScore = 3
 }
 process {
   log "Part 1 Answer:" (get-content $InputFile1 | Get-Part1Answer)
