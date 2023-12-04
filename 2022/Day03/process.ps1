5d878cd169a69e8f56a0b69391c48a178b97457a
Dale Hirt <2246127+dalehhirt@users.noreply.github.com>
2023-12-03 16:29:59 -0800

    feat: part 1 of day 3 of 2022

--- 2022/Day03/process.ps1
+++ 2022/Day03/process.ps1
@@ -7,6 +7,35 @@ https://adventofcode.com/2022/day/3
 [CmdletBinding(SupportsShouldProcess=$true)]
 param()
 begin {
+  function Get-CharValue {
+    [CmdletBinding()]
+    param (
+      [Parameter(ValueFromPipeline=$true)]
+      [string[]]
+      $lines
+    )
+    begin {
+      $returnValue = 0
+    }
+    process{
+      foreach ($line in $lines) {
+        log-verbose "Char $line"
+        if($null -ne ($byteValuesUpperCase.Keys | Where-Object {$_ -ceq $line})) {
+          $returnValue += $byteValuesUpperCase[[char]$line]
+          log-verbose "Value" $byteValuesUpperCase[[char]$line]
+        }
+        if($null -ne ($byteValuesLowerCase.Keys | Where-Object {$_ -ceq $line})) {
+          $returnValue += $byteValuesLowerCase[[char]$line]
+          log-verbose "Value" $byteValuesLowerCase[[char]$line]
+        }
+      }
+
+    }
+    end{
+      return $returnValue
+    }
+  }
+
   function Get-Part1Answer {
     [CmdletBinding()]
     param (
@@ -16,11 +45,25 @@ begin {
     )
     
     begin {
-      $returnValue = ""
+      $returnValue = 0
     }
     
     process {
       foreach ($line in $lines) {
+        $sameValues = @()
+        $halfLine = ($line.Length / 2)
+        $firstHalf = $line.Substring(0, $halfLine)
+        $secondHalf = $line.Substring($halfLine)
+        for ($i = 0; $i -lt $halfLine; $i++) {
+          for ($j = 0; $j -lt $halfLine; $j++) {
+            if($firstHalf[$i] -ceq $secondHalf[$j]) {
+              $sameValues += $($firstHalf[$i])
+            }
+          }
+        }
+        # Only need to specify type once
+        $SameValues = $SameValues | Select-Object -Unique
+        $returnValue += $SameValues | Get-CharValue 
       }
     }
     
@@ -61,6 +104,20 @@ begin {
   log-verbose "Input file 1 path: $InputFile1"
   $InputFile2 = Resolve-Path (Join-Path $PSScriptRoot "input2.txt")
   log-verbose "Input file 2 path: $InputFile2"
+
+  $byteValuesLowerCase = @{}
+  $priorityStart = 1
+  'a'..'z' | foreach {
+    $byteValuesLowerCase[$_] = $priorityStart
+    $priorityStart++
+  }
+
+  $byteValuesUpperCase = @{}
+  $priorityStart = 27
+  'A'..'Z' | foreach {
+    $byteValuesUpperCase[$_] = $priorityStart
+    $priorityStart++
+  }
 }
 process {
   log "Part 1 Answer:" (get-content $InputFile1 | Get-Part1Answer)
