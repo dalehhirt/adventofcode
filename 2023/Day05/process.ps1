181a70e11ecd4456cc5bf4590fc7f39516448aba
Dale Hirt <2246127+dalehhirt@users.noreply.github.com>
2023-12-06 22:19:34 -0800

    feat: part 1 of day 5 of 2023

--- 2023/Day05/process.ps1
+++ 2023/Day05/process.ps1
@@ -7,24 +7,116 @@ This script runs.
 [CmdletBinding(SupportsShouldProcess=$true)]
 param()
 begin {
-  function Process-Part1 {
+  class Map {
+    [string] $SourceMap
+    [string] $DestinationMap
+    [object] $SinglePoints
+    Map() {
+      $this.SinglePoints = @{}
+    }
+  }
+
+  function Add-KeyValuePair {
     [CmdletBinding()]
     param (
-      $line
+      $hashTable,
+      $key,
+      $value
     )
     
     begin {
-      $returnValue = 0
+      
     }
     
     process {
+      $found = $hashTable.Keys | Where-Object {$_ -eq $key}
+      if($null -eq $found) {
+        $hashTable.Add($key, $value)
+      }
+      else {
+        $hashTable[$key] = $value
+      }
+    }
+    
+    end {
       
+    }
+  }
+  function Create-Map {
+    param (
+      [string]$MapName,
+      [string[]]$ranges
+    )
+    begin {
+      $mappings = $MapName -split "-to-"
+      $sourceMap = $mappings[0]
+      $destinationMap = $mappings[1]
+      
+      $returnValue = @{$sourceMap = [Map]::new()}
+
+      $returnValue[$sourceMap].SourceMap = $sourceMap
+      $returnValue[$sourceMap].DestinationMap = $destinationMap
+    }
+    
+    process {
+      log-verbose "Parsing for map $sourceMap to $destinationMap"
+      foreach ($range in $ranges) {
+        $splitLine = $range -split " "
+        $numberRange = ([int]$splitLine[2])
+        for ($i = 0; $i -lt $numberRange; $i++) {
+          Add-KeyValuePair -hashTable $returnValue[$sourceMap].SinglePoints -key (([int]$splitLine[1]) + $i) -value (([int]$splitLine[0]) + $i)
+        }
+      }
     }
     
     end {
       return $returnValue
     }
   }
+
+  function Process-Part1 {
+    [CmdletBinding()]
+    param (
+      $seeds,
+      $maps
+    )
+    
+    begin {
+      $returnValue = 0
+      $currentSet = "seed"
+      $currentValues = $seeds
+    }
+    
+    process {
+      do {
+        log-verbose "Checking against $currentSet map"
+        $currentMap = $maps[$currentSet]
+        $newValues = @()
+        $currentValues | foreach {
+          $currentValue = $_
+          if($null -ne ($currentMap.SinglePoints.Keys | Where-Object {$_ -eq $currentValue})) {
+            $newValues += @($currentMap.SinglePoints[[int]$currentValue])
+          }
+          else {
+            $newValues += @($currentValue)
+          }
+        }
+        $currentValues = $newValues
+
+        log-verbose "Moving to $($currentMap.DestinationMap) map"
+        $currentSet = $currentMap.DestinationMap
+      } until (
+        $currentSet -eq "location"
+      )
+      
+      $returnValue = $currentValues | Sort-Object | Select-Object -first 1
+    }
+    
+    end {
+      return $returnValue
+    }
+  }
+  
   function Get-Part1Answer {
     [CmdletBinding()]
     param (
@@ -35,15 +127,38 @@ begin {
     
     begin {
       $returnValue = 0
+      $seeds = @()
+      $maps = @{}
+      $currentMapName = ""
+      $currentMapLines = @()
     }
     
     process {
       foreach ($line in $lines) {
-        $returnValue += Process-Part1 -line $line
+        if([regex]::match($line, "^seeds:").Success -eq $true){
+          $seeds += $line.Replace("seeds:", "") -split " " | Where-Object{$_ -ne ""}
+        }
+        elseif([regex]::match($line, "^.*\smap:").Success -eq $true){
+          if($currentMapName -ne "") {
+            log-verbose "Creating map for $currentMapName"
+            $maps += Create-Map -MapName $currentMapName -ranges $currentMapLines
+            $currentMapLines = @()
+          }
+          $currentMapName = $line.Replace(" map:", "")
+        }
+        elseif ($line -ne "") {
+          $currentMapLines += $line
+        }
+        #$returnValue += Process-Part1 -line $line
       }
     }
     
     end {
+      # one last time to account for last set of values
+      log-verbose "Creating map for $currentMapName"
+      $maps += Create-Map -MapName $currentMapName -ranges $currentMapLines
+
+      $returnValue = Process-Part1 -seeds $seeds -maps $maps
       return $returnValue
     }
   }
