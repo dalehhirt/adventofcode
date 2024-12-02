bc9d392b4ede0f7e73fee018dbb5a5366a14b249
Dale Hirt <2246127+dalehhirt@users.noreply.github.com>
2024-12-02 11:47:37 -0800

    fix: part 1 answered

--- 2024/Day02/process.ps1
+++ 2024/Day02/process.ps1
@@ -2,11 +2,23 @@
 .Description
 This script runs.
 .LINK
-<Replace with link to day problem>
+https://adventofcode.com/2024/day/2
 #>
 [CmdletBinding(SupportsShouldProcess=$true)]
 param()
 begin {
+  function Is-Increasing() {
+    param ($val1, $val2)
+
+    # if greater than or equal, by default it's false
+    $returnValue = $false
+
+    if ($splitLineArray[0] -lt $splitLineArray[1]) {
+      $returnValue = $true
+    }
+
+    return $returnValue
+}
   function Process-Part1 {
     [CmdletBinding()]
     param (
@@ -14,15 +26,53 @@ begin {
     )
     
     begin {
-      $returnValue = 0
+      $isIncreasing = $false
+      $isSafe = $true
     }
     
     process {
+      $splitLineArray = $line.split(" ") | ForEach-Object {[int]$_}
+      for ($i = 0; $i -lt $splitLineArray.Count; $i++) {
+        if ($i -eq 0) {
+          # Do nothing and move on to the next value
+          continue
+        }
+        elseif ($i -eq 1) {
+          # Determine if initial direction increasing or decreasing
+          $isIncreasing = Is-Increasing -val1 $splitLineArray[0] -val2 $splitLineArray[1]
+        }
+
+        $valPrevious = $splitLineArray[$i - 1]
+        $valCurrent = $splitLineArray[$i]
+        # The levels are either all increasing or all decreasing.
+        if($isIncreasing -ne (Is-Increasing -val1 $valPrevious -val2 $valCurrent)) {
+          $isSafe = $false
+        }
+
+        # Any two adjacent levels differ by at least one and at most three.
+        $diffVal = -1
+        if($isIncreasing) {
+          $diffVal = $valCurrent - $valPrevious
+        }
+        else {
+          $diffVal = $valPrevious - $valCurrent
+        }
+
+        if($diffVal -notin (1,2,3)) {
+          $isSafe = $false
+        }
+        
+      }
       
     }
     
     end {
-      return $returnValue
+      if ($isSafe) {
+        return 1
+      }
+      else {
+        return 0
+      }
     }
   }
   function Get-Part1Answer {
