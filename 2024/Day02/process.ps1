2f0ed5979d19020756f00e439ba3c775a89e56bd
Dale Hirt <2246127+dalehhirt@users.noreply.github.com>
2024-12-03 05:42:04 -0800

    fix: redid part 1 answer to better get part 2 answer

--- 2024/Day02/process.ps1
+++ 2024/Day02/process.ps1
@@ -7,27 +7,6 @@ https://adventofcode.com/2024/day/2
 [CmdletBinding(SupportsShouldProcess=$true)]
 param()
 begin {
-  enum State {
-    Increasing
-    Decreasing
-    Same
-  }
-
-  function Is-Increasing() {
-    param ($val1, $val2)
-
-    # if greater than or equal, by default it's false
-    $returnValue = [State]::Decreasing
-
-    if ($val1 -lt $val2) {
-      $returnValue = [State]::Increasing
-    }
-    elseif ($val1 -eq $val2) {
-      $returnValue = [State]::Same
-    }
-
-    return $returnValue
-}
   function Process-Part1 {
     [CmdletBinding()]
     param (
@@ -35,51 +14,57 @@ begin {
     )
     
     begin {
-      $isIncreasing = [State]::Decreasing
-      $isSafe = $true
+      $diffSignCounts =  @(0) * 3 # [0] stores decreasing, [1] stores zero, [2] stores increasing
+      $levelDiffs =  @(0) * 32; # Stores the pairwise differences of the numbers in the list
+      $largeDiffIndexes =  @(0) * 32; # Stores the indexes of any differences that are too large
+
+      $returnValue = 0
     }
     
     process {
-      $splitLineArray = $line.split(" ") | ForEach-Object {[int]$_}
-      for ($i = 0; $i -lt $splitLineArray.Count; $i++) {
-        if ($i -eq 0) {
-          # Do nothing and move on to the next value
-          continue
-        }
-        elseif ($i -eq 1) {
-          # Determine if initial direction increasing or decreasing
-          $isIncreasing = Is-Increasing -val1 $splitLineArray[0] -val2 $splitLineArray[1]
+      $splitLineArray = [System.Collections.ArrayList]($line.split(" ") | ForEach-Object {[int]$_})
+
+      $i = 0
+      while($i -lt $splitLineArray.Count) {
+        $numDiffs = 0;
+        $largeDiffs = 0;
+
+        $prev = $splitLineArray[$i++]
+
+        while($i -lt $splitLineArray.Count) {
+          $number = $splitLineArray[$i++]
+
+          $diff = $number - $prev
+          $diffSign = [System.Math]::Sign($diff) + 1
+          $diffSignCounts[$diffSign]++
+          if(($diff -lt -3) -or ($diff -gt 3)){
+            $largeDiffIndexes[$largeDiffs++] = $numDiffs
+          }
+
+          $levelDiffs[$numDiffs++] = $diff
+          $prev = $number
         }
 
-        $valPrevious = $splitLineArray[$i - 1]
-        $valCurrent = $splitLineArray[$i]
-        $valEqual = $valPrevious -eq $valCurrent
-        # The levels are either all increasing or all decreasing.
-        if($valEqual -or ($isIncreasing -ne (Is-Increasing -val1 $valPrevious -val2 $valCurrent))) {
-          $isSafe = $false
-        }
+        $decreaseCount = $diffSignCounts[0];
+        $increaseCount = $diffSignCounts[2];
 
-        # Any two adjacent levels differ by at least one and at most three.
-        $diffVal = -1
-        if($isIncreasing -eq [State]::Increasing) {
-          $diffVal = $valCurrent - $valPrevious
-        }
-        else {
-          $diffVal = $valPrevious - $valCurrent
-        }
+        if (($decreaseCount -eq $numDiffs) -or ($increaseCount -eq $numDiffs))
+        {
+            if ($largeDiffs -eq 0)
+            {
+                $returnValue++;
+            }
 
-        if($diffVal -notin (1,2,3)) {
-          $isSafe = $false
+            0..($numDiffs - 1) | 
+              Where-Object {$levelDiffs[$_] -eq 0} | 
+              ForEach-Object {$returnValue = 0}
+
+            continue;
         }
       }
     }
     end {
-      if ($isSafe) {
-        return 1
-      }
-      else {
-        return 0
-      }
+      return $returnValue
     }
   }
   function Get-Part1Answer {
@@ -112,95 +97,156 @@ begin {
     )
     
     begin {
-      $isIncreasing = $true
-      $isSafe = $true
-      $problemDampener = 0
-      $originalLine = $line
+      $diffSignCounts =  @(0) * 3 # [0] stores decreasing, [1] stores zero, [2] stores increasing
+      # $lastDiffWithSignIndex =  @(0) * 3 # For each sign type, stores the index of the last difference we saw with it
+      $levelDiffs =  @(0) * 32; # Stores the pairwise differences of the numbers in the list
+      $largeDiffIndexes =  @(0) * 32; # Stores the indexes of any differences that are too large
+
+      $returnValue = 0
+      # $part2 = 0
     }
     
     process {
       $splitLineArray = [System.Collections.ArrayList]($line.split(" ") | ForEach-Object {[int]$_})
 
-      while($true) {
-        #log "Line count:" $splitLineArray.Count
-        $originalCount = $splitLineArray.Count
-        [System.Collections.ArrayList]$isIncreasingArray = @() 
-        for ($i = 0; $i -lt $splitLineArray.Count; $i++) {
-          if($i -eq 0){
-            continue
-          }
-          $isIncreasingArray.Add($splitLineArray[$i] -eq $splitLineArray[$i - 1] ? 
-                                  [State]::Same :
-                                  ($splitLineArray[$i] -gt $splitLineArray[$i - 1] ? 
-                                    [State]::Increasing : 
-                                    [State]::Decreasing)) | Out-Null
-        }
-  
-        $increasingCount = ($isIncreasingArray | Where-Object {$_ -eq [State]::Increasing} | Measure-Object).Count
-        $decreasingCount = ($isIncreasingArray | Where-Object {$_ -eq [State]::Decreasing} | Measure-Object).Count
+      $i = 0
+      while($i -lt $splitLineArray.Count) {
+        $numDiffs = 0;
+        $largeDiffs = 0;
 
-        $isIncreasing = $increasingCount -gt $decreasingCount ? [State]::Increasing : [State]::Decreasing
+        $prev = $splitLineArray[$i++]
 
-        for ($i = 0; $i -lt $splitLineArray.Count; $i++) {
-          if ($i -eq 0) {
-            # Do nothing and move on to the next value
-            continue
+        while($i -lt $splitLineArray.Count) {
+          $number = $splitLineArray[$i++]
+          #log "Checking $prev $number"
+
+          $diff = $number - $prev
+          $diffSign = [System.Math]::Sign($diff) + 1
+          #log $diffSign
+          $diffSignCounts[$diffSign]++
+          # $lastDiffWithSignIndex[$diffSign] = $numDiffs
+          if(($diff -lt -3) -or ($diff -gt 3)){
+            $largeDiffIndexes[$largeDiffs++] = $numDiffs
           }
 
-          $valPrevious = $splitLineArray[$i - 1]
-          $valCurrent = $splitLineArray[$i]
-          $valEqual = $valPrevious -eq $valCurrent
-          # The levels are either all increasing or all decreasing.
-          if($valEqual -or ($isIncreasing -ne (Is-Increasing -val1 $valPrevious -val2 $valCurrent))) {
-            if ($problemDampener -eq 0) {
-              $problemDampener += 1
-              $splitLineArray.RemoveAt($i - 1)
-              break
-            }
-            else {
-              $isSafe = $false
-            }
-          }
-
-          # Any two adjacent levels differ by at least one and at most three.
-          $diffVal = -1
-          if($isIncreasing -eq 'Increasing') {
-            $diffVal = $valCurrent - $valPrevious
-          }
-          else {
-            $diffVal = $valPrevious - $valCurrent
-          }
-
-          if($diffVal -notin (1,2,3)) {
-            if ($problemDampener -eq 0) {
-              $problemDampener += 1
-              $splitLineArray.RemoveAt($i)
-              break
-            }
-            else {
-              $isSafe = $false
-            }
-          }
+          $levelDiffs[$numDiffs++] = $diff
+          $prev = $number
         }
 
-        if ($originalCount -eq $splitLineArray.Count) {
-          break
+        $decreaseCount = $diffSignCounts[0];
+        $increaseCount = $diffSignCounts[2];
+
+        if (($decreaseCount -eq $numDiffs) -or ($increaseCount -eq $numDiffs))
+        {
+            if ($largeDiffs -eq 0)
+            {
+              $returnValue++;
+
+              0..($numDiffs - 1) | 
+                Where-Object {$levelDiffs[$_] -eq 0} | 
+                ForEach-Object {$returnValue = 0}
+            }
+            elseif ($largeDiffs -eq 1)
+            {
+                # Only can work if it starts or ends with the large difference
+                $largeDiffIndex = $largeDiffIndexes[0];
+                if (($largeDiffIndex -eq 0) -or ($largeDiffIndex -eq ($numDiffs - 1))) {
+                  $returnValue++;
+                }
+            }
+            continue;
         }
+
+        # Get the index of the single difference going in the wrong direction
+        # $expectedSign = 0;
+        # $indexToRemove = 0;
+        # if ($decreaseCount -eq ($numDiffs - 1))
+        # {
+        #     $expectedSign = -1;
+        #     $indexToRemove = $increaseCount -eq 1 ? $lastDiffWithSignIndex[2] : $lastDiffWithSignIndex[1]; #either increasing or zero
+        # }
+        # elseif ($increaseCount -eq ($numDiffs - 1))
+        # {
+        #     $expectedSign = 1;
+        #     $indexToRemove = $decreaseCount -eq 1 ? $lastDiffWithSignIndex[0] : $lastDiffWithSignIndex[1]; # either decreasing or zero
+        # }
+        # else
+        # {
+        #     # Too many differences in the wrong direction, skip
+        #     continue;
+        # }
+
+        # Our next goal is to identify which side of the difference should be removed to ensure that the resulting sequence is valid
+        # We can easily calculate the new difference are removal by adding together adjacent differences: (a - b) + (b - c) == a - c
+
+        # If there are large differences involved, we may be forced to choose either the left or right side
+        # $forcedMergeIndex = -1;
+        # if ($largeDiffs -eq 1)
+        # {
+        #     # Must be either the same as indexToRemove or immediately next to merge
+        #     $distance = $largeDiffIndexes[0] - $indexToRemove;
+        #     if (($distance -eq -1) -or ($distance -eq 1)) {
+        #         $forcedMergeIndex = $indexToRemove + $distance;
+        #     }
+        #     elseif ($distance -ne 0) {
+        #       continue;
+        #     }
+        # }
+        # elseif ($largeDiffs -eq 2)
+        # {
+        #     # one must be the indexToRemove, the other must be immediately before or after it
+        #     $distance1 = $largeDiffIndexes[0] - $indexToRemove;
+        #     $distance2 = $largeDiffIndexes[1] - $indexToRemove;
+        #     if (($distance1 -eq 0) -and ($distance2 -eq 1)){
+        #       $forcedMergeIndex = $indexToRemove + 1;
+        #     }
+        #     elseif (($distance1 -eq -1) -and ($distance2 -eq 0)){
+        #       $forcedMergeIndex = $indexToRemove - 1;
+        #     }
+        #     else {
+        #       continue;
+        #     }
+        # }
+        # elseif ($largeDiffs -gt 2)
+        # {
+        #     continue;
+        # }
+
+        # # Now test the left and right sides to see if the new sequence is valid
+        # $diffToRemove = $levelDiffs[$indexToRemove];
+        # if ($forcedMergeIndex -ne -1) # handle forced case
+        # {
+        #   $val = $expectedSign * ($diffToRemove + $levelDiffs[$forcedMergeIndex])
+        #   if (($val -gt 0) -and ($val -le 3)) {
+        #     $part2++;
+        #   }
+        # }
+        # elseif (($indexToRemove -eq 0) -or 
+        #     ($indexToRemove -eq ($numDiffs - 1)) -or
+        #     (
+        #       (
+        #         ($expectedSign * ($diffToRemove + $levelDiffs[$indexToRemove - 1])) -gt 0
+        #       ) -and 
+        #       (
+        #         ($expectedSign * ($diffToRemove + $levelDiffs[$indexToRemove - 1])) -le 3
+        #       )
+        #     ) -or
+        #     (
+        #       (
+        #         ($expectedSign * (diffToRemove + levelDiffs[indexToRemove + 1])) -gt 0
+        #       ) -and 
+        #       (
+        #         ($expectedSign * (diffToRemove + levelDiffs[indexToRemove + 1])) -le 3
+        #       )
+        #     )
+        # )
+        # {
+        #     $part2++;
+        # }
       }
-
-      #if(!$isSafe) {
-        log "Safe ($isSafe) PD ($($problemDampener)):"
-        log "  Original:" $originalLine 
-        log "  Final:" ($splitLineArray.ToArray() -join " ")
-      #}
     }
     end {
-      if ($isSafe) {
-        return 1
-      }
-      else {
-        return 0
-      }
+      return $returnValue
     }
   }
 
