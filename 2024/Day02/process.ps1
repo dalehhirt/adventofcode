87b5ff5a232e27662ae85445c0a7e7dc6d212c47
Dale Hirt <2246127+dalehhirt@users.noreply.github.com>
2024-12-02 17:12:33 -0800

    fix: nowhere near my part 2 solution

--- 2024/Day02/process.ps1
+++ 2024/Day02/process.ps1
@@ -7,14 +7,23 @@ https://adventofcode.com/2024/day/2
 [CmdletBinding(SupportsShouldProcess=$true)]
 param()
 begin {
+  enum State {
+    Increasing
+    Decreasing
+    Same
+  }
+
   function Is-Increasing() {
     param ($val1, $val2)
 
     # if greater than or equal, by default it's false
-    $returnValue = $false
+    $returnValue = [State]::Decreasing
 
-    if ($splitLineArray[0] -lt $splitLineArray[1]) {
-      $returnValue = $true
+    if ($val1 -lt $val2) {
+      $returnValue = [State]::Increasing
+    }
+    elseif ($val1 -eq $val2) {
+      $returnValue = [State]::Same
     }
 
     return $returnValue
@@ -26,7 +35,7 @@ begin {
     )
     
     begin {
-      $isIncreasing = $false
+      $isIncreasing = [State]::Decreasing
       $isSafe = $true
     }
     
@@ -44,14 +53,15 @@ begin {
 
         $valPrevious = $splitLineArray[$i - 1]
         $valCurrent = $splitLineArray[$i]
+        $valEqual = $valPrevious -eq $valCurrent
         # The levels are either all increasing or all decreasing.
-        if($isIncreasing -ne (Is-Increasing -val1 $valPrevious -val2 $valCurrent)) {
+        if($valEqual -or ($isIncreasing -ne (Is-Increasing -val1 $valPrevious -val2 $valCurrent))) {
           $isSafe = $false
         }
 
         # Any two adjacent levels differ by at least one and at most three.
         $diffVal = -1
-        if($isIncreasing) {
+        if($isIncreasing -eq [State]::Increasing) {
           $diffVal = $valCurrent - $valPrevious
         }
         else {
@@ -61,11 +71,8 @@ begin {
         if($diffVal -notin (1,2,3)) {
           $isSafe = $false
         }
-        
       }
-      
     }
-    
     end {
       if ($isSafe) {
         return 1
@@ -105,15 +112,95 @@ begin {
     )
     
     begin {
-      $returnValue = 0
+      $isIncreasing = $true
+      $isSafe = $true
+      $problemDampener = 0
+      $originalLine = $line
     }
     
     process {
-      
+      $splitLineArray = [System.Collections.ArrayList]($line.split(" ") | ForEach-Object {[int]$_})
+
+      while($true) {
+        #log "Line count:" $splitLineArray.Count
+        $originalCount = $splitLineArray.Count
+        [System.Collections.ArrayList]$isIncreasingArray = @() 
+        for ($i = 0; $i -lt $splitLineArray.Count; $i++) {
+          if($i -eq 0){
+            continue
+          }
+          $isIncreasingArray.Add($splitLineArray[$i] -eq $splitLineArray[$i - 1] ? 
+                                  [State]::Same :
+                                  ($splitLineArray[$i] -gt $splitLineArray[$i - 1] ? 
+                                    [State]::Increasing : 
+                                    [State]::Decreasing)) | Out-Null
+        }
+  
+        $increasingCount = ($isIncreasingArray | Where-Object {$_ -eq [State]::Increasing} | Measure-Object).Count
+        $decreasingCount = ($isIncreasingArray | Where-Object {$_ -eq [State]::Decreasing} | Measure-Object).Count
+
+        $isIncreasing = $increasingCount -gt $decreasingCount ? [State]::Increasing : [State]::Decreasing
+
+        for ($i = 0; $i -lt $splitLineArray.Count; $i++) {
+          if ($i -eq 0) {
+            # Do nothing and move on to the next value
+            continue
+          }
+
+          $valPrevious = $splitLineArray[$i - 1]
+          $valCurrent = $splitLineArray[$i]
+          $valEqual = $valPrevious -eq $valCurrent
+          # The levels are either all increasing or all decreasing.
+          if($valEqual -or ($isIncreasing -ne (Is-Increasing -val1 $valPrevious -val2 $valCurrent))) {
+            if ($problemDampener -eq 0) {
+              $problemDampener += 1
+              $splitLineArray.RemoveAt($i - 1)
+              break
+            }
+            else {
+              $isSafe = $false
+            }
+          }
+
+          # Any two adjacent levels differ by at least one and at most three.
+          $diffVal = -1
+          if($isIncreasing -eq 'Increasing') {
+            $diffVal = $valCurrent - $valPrevious
+          }
+          else {
+            $diffVal = $valPrevious - $valCurrent
+          }
+
+          if($diffVal -notin (1,2,3)) {
+            if ($problemDampener -eq 0) {
+              $problemDampener += 1
+              $splitLineArray.RemoveAt($i)
+              break
+            }
+            else {
+              $isSafe = $false
+            }
+          }
+        }
+
+        if ($originalCount -eq $splitLineArray.Count) {
+          break
+        }
+      }
+
+      #if(!$isSafe) {
+        log "Safe ($isSafe) PD ($($problemDampener)):"
+        log "  Original:" $originalLine 
+        log "  Final:" ($splitLineArray.ToArray() -join " ")
+      #}
     }
-    
     end {
-      return $returnValue
+      if ($isSafe) {
+        return 1
+      }
+      else {
+        return 0
+      }
     }
   }
 
