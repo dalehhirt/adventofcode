84e18a086e1601bddc3e85332eef1aa0f175357e
Dale Hirt <2246127+dalehhirt@users.noreply.github.com>
2024-12-03 11:00:16 -0800

    feat: initial 2015 code

---
+++ 2015/Day13/process.ps1
@@ -0,0 +1,122 @@
+<#
+.Description
+This script runs.
+.LINK
+<Replace with link to day problem>
+#>
+[CmdletBinding(SupportsShouldProcess=$true)]
+param()
+begin {
+  function Process-Part1 {
+    [CmdletBinding()]
+    param (
+      $line
+    )
+    
+    begin {
+      $returnValue = 0
+    }
+    
+    process {
+      
+    }
+    
+    end {
+      return $returnValue
+    }
+  }
+  function Get-Part1Answer {
+    [CmdletBinding()]
+    param (
+      [Parameter(ValueFromPipeline=$true)]
+      [string[]]
+      $lines
+    )
+    
+    begin {
+      $returnValue = 0
+    }
+    
+    process {
+      foreach ($line in $lines) {
+        $returnValue += Process-Part1 -line $line
+      }
+    }
+    
+    end {
+      return $returnValue
+    }
+  }
+
+  function Process-Part2 {
+    [CmdletBinding()]
+    param (
+      $line
+    )
+    
+    begin {
+      $returnValue = 0
+    }
+    
+    process {
+      
+    }
+    
+    end {
+      return $returnValue
+    }
+  }
+
+  function Get-Part2Answer {
+    [CmdletBinding()]
+    param (
+      [Parameter(ValueFromPipeline=$true)]
+      [string[]]
+      $lines
+    )
+    
+    begin {
+      $returnValue = 0
+    }
+    
+    process {
+      foreach ($line in $lines) {
+        $returnValue += Process-Part2 -line $line
+      }
+    }
+    
+    end {
+      return $returnValue
+    }
+  }
+
+  #-----------------
+  # Helper functions
+  Import-Module $PSScriptRoot\..\..\modules\AdventOfCode.Util -Force -verbose:$false -DisableNameChecking
+
+  #-----------------
+  # Global Variables
+  $InputFile1 = Resolve-Path (Join-Path $PSScriptRoot "input1.txt")
+  log-verbose "Input file 1 path: $InputFile1"
+  $InputFile2 = Resolve-Path (Join-Path $PSScriptRoot "input2.txt")
+  log-verbose "Input file 2 path: $InputFile2"
+}
+process {
+  if((get-content $InputFile1) -eq "Placeholder input text file") {
+    log "Input 1 data does not exist yet."
+  }
+  else {
+    log "Processing Part 1..."
+    log "Part 1 Answer:" (get-content $InputFile1 | Get-Part1Answer)
+  }
+
+  if((get-content $InputFile2) -eq "Placeholder input text file") {
+    log "Input 2 data does not exist yet."
+  }
+  else {
+    log "Processing Part 2..."
+    log "Part 2 Answer:" (get-content $InputFile2 | Get-Part2Answer)
+  }
+}
+end {
+}
