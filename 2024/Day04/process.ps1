a14d90804f047bd7b34f7ceb7f9508787c2a445b
Dale Hirt <2246127+dalehhirt@users.noreply.github.com>
2024-12-04 10:39:03 -0800

    feat: part 1 day 4 2024 initial

--- 2024/Day04/process.ps1
+++ 2024/Day04/process.ps1
@@ -2,23 +2,153 @@
 .Description
 This script runs.
 .LINK
-<Replace with link to day problem>
+https://adventofcode.com/2024/day/4
 #>
 [CmdletBinding(SupportsShouldProcess=$true)]
 param()
 begin {
+
+  function Match-Value {
+    [CmdletBinding()]
+    param (
+      [string[]]
+      $lines,
+      [int]
+      $line,
+      [int]
+      $index,
+      [string]
+      $value
+    )
+    
+    begin {
+      $returnValue = $false
+    }
+    
+    process {
+      if ($lines[$line - 1][$index] -eq $value) {
+        $returnValue = $true
+      }
+    }
+    
+    end {
+      return $returnValue
+    }
+  }
+
   function Process-Part1 {
     [CmdletBinding()]
     param (
-      $line
+      [string[]]
+      $lines,
+      [object[]]
+      $matchLines
     )
     
     begin {
       $returnValue = 0
+      $totalLength = 4
+      $minIndex = $totalLength - 1
+      $maxIndex = $lines[0].Length - $totalLength -1
+      $minLine = $totalLength
+      $maxLine = $lines.Count - $totalLength
     }
     
     process {
-      
+      foreach ($matchLine in $matchLines) {
+        # There are 8 directions to check for:
+
+        # 1. Up
+        if ($matchLine.Line -ge $minLine) {
+          <# Action to perform if the condition is true #>
+          if(Match-Value -lines $lines -line ($matchLine.Line - 1) -index $matchLine.Index -value "M") {
+            if(Match-Value -lines $lines -line ($matchLine.Line - 2) -index $matchLine.Index -value "A") {
+              if(Match-Value -lines $lines -line ($matchLine.Line - 3) -index $matchLine.Index -value "S") {
+                  $returnValue++
+                  log-verbose "Match found at line $($matchLine.Line) and index $($matchLine.Index) up"
+              }
+            }
+          }
+        }
+        # 2. Down
+        if ($matchLine.Line -le $maxLine) {
+          if(Match-Value -lines $lines -line ($matchLine.Line + 1) -index $matchLine.Index -value "M") {
+            if(Match-Value -lines $lines -line ($matchLine.Line + 2) -index $matchLine.Index -value "A") {
+              if(Match-Value -lines $lines -line ($matchLine.Line + 3) -index $matchLine.Index -value "S") {
+                  $returnValue++
+                  log-verbose "Match found at line $($matchLine.Line) and index $($matchLine.Index) down"
+              }
+            }
+          }
+        }
+        # 3. Left
+        if ($matchLine.index -ge $minIndex) {
+          if(Match-Value -lines $lines -line ($matchLine.Line) -index ($matchLine.Index - 1) -value "M") {
+            if(Match-Value -lines $lines -line ($matchLine.Line) -index ($matchLine.Index - 2) -value "A") {
+              if(Match-Value -lines $lines -line ($matchLine.Line) -index ($matchLine.Index - 3) -value "S") {
+                  $returnValue++
+                  log-verbose "Match found at line $($matchLine.Line) and index $($matchLine.Index) left"
+              }
+            }
+          }
+        }
+        # 4. Right
+        if ($matchLine.index -le $maxIndex) {
+          if(Match-Value -lines $lines -line ($matchLine.Line) -index ($matchLine.Index + 1) -value "M") {
+            if(Match-Value -lines $lines -line ($matchLine.Line) -index ($matchLine.Index + 2) -value "A") {
+              if(Match-Value -lines $lines -line ($matchLine.Line) -index ($matchLine.Index + 3) -value "S") {
+                  $returnValue++
+                  log-verbose "Match found at line $($matchLine.Line) and index $($matchLine.Index) right"
+              }
+            }
+          }
+        }
+        # 5. Up-Left
+        if ($matchLine.line -ge $minLine -and $matchLine.index -ge $minIndex) {
+          if(Match-Value -lines $lines -line ($matchLine.Line - 1) -index ($matchLine.Index - 1) -value "M") {
+            if(Match-Value -lines $lines -line ($matchLine.Line - 2) -index ($matchLine.Index - 2) -value "A") {
+              if(Match-Value -lines $lines -line ($matchLine.Line - 3) -index ($matchLine.Index - 3) -value "S") {
+                  $returnValue++
+                  log-verbose "Match found at line $($matchLine.Line) and index $($matchLine.Index) up-left"
+              }
+            }
+          }
+        }
+        # 6. Up-Right
+        if ($matchLine.line -ge $minLine -and $matchLine.index -le $maxIndex) {
+          if(Match-Value -lines $lines -line ($matchLine.Line - 1) -index ($matchLine.Index + 1) -value "M") {
+            if(Match-Value -lines $lines -line ($matchLine.Line - 2) -index ($matchLine.Index + 2) -value "A") {
+              if(Match-Value -lines $lines -line ($matchLine.Line - 3) -index ($matchLine.Index + 3) -value "S") {
+                  $returnValue++
+                  log-verbose "Match found at line $($matchLine.Line) and index $($matchLine.Index) up-right"
+              }
+            }
+          }
+        }
+        # 7. Down-Left
+        if ($matchLine.line -le $maxLine -and $matchLine.index -ge $minIndex) {
+          if(Match-Value -lines $lines -line ($matchLine.Line + 1) -index ($matchLine.Index - 1) -value "M") {
+            if(Match-Value -lines $lines -line ($matchLine.Line + 2) -index ($matchLine.Index - 2) -value "A") {
+              if(Match-Value -lines $lines -line ($matchLine.Line + 3) -index ($matchLine.Index - 3) -value "S") {
+                  $returnValue++
+                  log-verbose "Match found at line $($matchLine.Line) and index $($matchLine.Index) down-left"
+              }
+            }
+          }
+        }
+
+        # 8. Down-Right
+        if ($matchLine.line -le $maxLine -and $matchLine.index -le $maxIndex) {
+          if(Match-Value -lines $lines -line ($matchLine.Line + 1) -index ($matchLine.Index + 1) -value "M") {
+            if(Match-Value -lines $lines -line ($matchLine.Line + 2) -index ($matchLine.Index + 2) -value "A") {
+              if(Match-Value -lines $lines -line ($matchLine.Line + 3) -index ($matchLine.Index + 3) -value "S") {
+                  $returnValue++
+                  log-verbose "Match found at line $($matchLine.Line) and index $($matchLine.Index) down-right"
+              }
+            }
+          }
+        }
+      }
     }
     
     end {
@@ -35,15 +165,45 @@ begin {
     
     begin {
       $returnValue = 0
+      $beginningSymbol = "X"
+      $lineNumber = 0
+      $lineLength = 0
+      $copyLines = @()
+      $lineMatches = @()
     }
     
     process {
-      foreach ($line in $lines) {
-        $returnValue += Process-Part1 -line $line
+      $lineMatches += foreach ($line in $lines) {
+        $lineNumber++
+        
+        if (1 -eq $lineNumber) {
+          $lineLength = $line.Length
+        }
+        else {
+          if ($lineLength -ne $line.Length) {
+            throw "Line $($lineNumber) has a different length than the first line ($lineLength)." 
+          }
+        }
+
+        $copyLines += $line
+        
+        log-verbose "Line $($lineNumber):" $line
+        $matchesLine = ([regex]$beginningSymbol).Matches($line)
+        #log "Matches:" $matchesLine.count
+        $matchesLine | foreach {
+          $matchLine = $_
+          [PSCustomObject]@{
+            Index = $matchLine.Index
+            Line = $lineNumber
+          } 
+        }
       }
     }
     
     end {
+      $lineMatches | foreach {log-verbose $_.Line $_.Index}
+      log "Found $($lineMatches.Count) matches."
+      $returnValue = Process-Part1 -lines $copyLines -matchLines $lineMatches
       return $returnValue
     }
   }
