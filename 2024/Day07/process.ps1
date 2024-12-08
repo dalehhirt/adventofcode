e336056c7d1cf4a62b2e296a5a86662993164c40
Dale Hirt <2246127+dalehhirt@users.noreply.github.com>
2024-12-08 00:54:31 -0800

    fix: part 2 day 7 2024 rewrote as recursion

--- 2024/Day07/process.ps1
+++ 2024/Day07/process.ps1
@@ -2,14 +2,79 @@
 .Description
 This script runs.
 .LINK
-<Replace with link to day problem>
+https://adventofcode.com/2024/day/7
 #>
 [CmdletBinding(SupportsShouldProcess=$true)]
 param()
 begin {
+  $operatorsPart1 = @("*", "+")
+  $operatorsPart2 = $operatorsPart1 + @("||")
+
+  function Get-ArrayOfOperators(){
+    param([int]$count, [string[]]$operatorsToUse)
+
+    [System.Collections.ArrayList]$returnValue = @()
+
+    if ($count -eq 1) {
+      $returnValue = $operatorsToUse
+    }
+    else {
+      $oneLessArray = Get-ArrayOfOperators -Count ($count - 1) -operatorsToUse $operatorsToUse
+      $operatorsToUse | foreach {
+        $operator = $_
+        $oneLessArray | ForEach-Object {
+          $arrayVal = $_ + "," + $operator
+          $returnValue += $arrayVal
+        }
+      }
+
+    }
+
+    return $returnValue
+  }
+
+  function Process-Values {
+    [CmdletBinding()]
+    param (
+      [System.Collections.ArrayList]
+      $valuesToUse,
+      [System.Collections.ArrayList]
+      $operatorsToUse
+    )
+    
+    begin {
+      $returnValue = ""
+    }
+    
+    process {
+      if($valuesToUse.Count -eq 1) {
+        $returnValue = $valuesToUse[0]
+      }
+      else {
+        $val = Process-Values -values $valuesToUse.GetRange(0, $valuesToUse.count - 1)  -operators $operatorsToUse.GetRange(0, $operatorsToUse.Count - 1)
+        $operator = $operatorsToUse[$operatorsToUse.Count - 1]
+
+        if ($operator -eq "||") {
+          $returnValue = "$val$($valuesToUse[$valuesToUse.count - 1])"
+        }
+        else {
+          $ToTest = "$val$operator$($valuesToUse[$valuesToUse.count - 1])"
+        
+          $returnValue = Invoke-Expression $ToTest
+          log "To Test:" $ToTest $returnValue
+          }
+      }
+    }
+    
+    end {
+      return $returnValue      
+    }
+  }
+
   function Process-Part1 {
     [CmdletBinding()]
     param (
+      [string]
       $line
     )
     
@@ -18,13 +83,32 @@ begin {
     }
     
     process {
-      
+      $separatorIndex = $line.IndexOf(":") 
+      $lineTotal = $line.Substring(0, $separatorIndex)
+
+      $lineValues = $line.Substring($separatorIndex + 1).Trim() -split " "
+      $neededOperators = $lineValues.Count - 1
+
+      $operatorsToTry = Get-ArrayOfOperators -count $neededOperators -operatorsToUse $operatorsPart1
+
+      log "Checking:" 
+      log "  line:" $line
+      foreach ($operators in $operatorsToTry) {
+        log "  operators:" $operators
+        $answer = Process-Values -values $lineValues -operators ($operators -split ",")
+        if($answer -eq $lineTotal) {
+          log "  Valid"
+          $returnValue = $answer
+          break
+        }
+      }
     }
     
     end {
       return $returnValue
     }
   }
+
   function Get-Part1Answer {
     [CmdletBinding()]
     param (
@@ -39,7 +123,7 @@ begin {
     
     process {
       foreach ($line in $lines) {
-        $returnValue += Process-Part1 -line $line
+        $returnValue += Process-Part1 $_
       }
     }
     
@@ -59,7 +143,25 @@ begin {
     }
     
     process {
-      
+      $separatorIndex = $line.IndexOf(":") 
+      $lineTotal = $line.Substring(0, $separatorIndex)
+
+      $lineValues = $line.Substring($separatorIndex + 1).Trim() -split " "
+      $neededOperators = $lineValues.Count - 1
+
+      $operatorsToTry = Get-ArrayOfOperators -count $neededOperators -operatorsToUse $operatorsPart2
+
+      log "Checking:" 
+      log "  line:" $line
+      foreach ($operators in $operatorsToTry) {
+        log "  operators:" $operators
+        $answer = Process-Values -values $lineValues -operators ($operators -split ",")
+        if($answer -eq $lineTotal) {
+          log "  Valid"
+          $returnValue = $answer
+          break
+        }
+      }
     }
     
     end {
@@ -90,6 +192,8 @@ begin {
     }
   }
 
+  Push-Location $PSScriptRoot
+
   #-----------------
   # Helper functions
   Import-Module $PSScriptRoot\..\..\modules\AdventOfCode.Util -Force -verbose:$false -DisableNameChecking
@@ -119,4 +223,5 @@ process {
   }
 }
 end {
+  Pop-Location
 }
